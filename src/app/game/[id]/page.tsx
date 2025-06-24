e75d62800c8dc81e32ea0706dbecc8f2db5d320d
Vladislav <developunions@gmail.com>
2025-06-24 14:04:33 +0300

    Update page.tsx

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -2,19 +2,12 @@
 
 import GameView from "@/components/game/GameView";
 
-// Тип для props страницы, Next.js передаст сюда `params`.
-type GamePageProps = {
-  params: {
-    id: string;
-  };
-};
-
-export default function GamePage({ params }: GamePageProps) {
+export default function GamePage({ params }: { params: { id: string } }) {
+  
   // Конвертируем id из строки в число
   const levelId = Number(params.id);
 
   // Проверяем, что ID является корректным числом.
-  // Эта проверка важна, если кто-то введет неверный URL.
   if (isNaN(levelId)) {
     return <div className="text-red-500 text-center p-10">Неверный ID уровня.</div>;
   }
@@ -22,7 +15,7 @@ export default function GamePage({ params }: GamePageProps) {
   return (
     <div className="container mx-auto p-4">
       <main className="my-8">
-        {/* ИСПРАВЛЕНО: GameView теперь получает только levelId, как и было запланировано */}
+        {/* GameView теперь получает только levelId */}
         <GameView levelId={levelId} />
       </main>
     </div>
