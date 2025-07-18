aa2aae02256dd9d7dc44557d7a67805e070d0f01
Vladislav <developunions@gmail.com>
2025-07-18 18:20:11 +0300

    Routes update

--- src/app/api/levels/[id]/hint/route.ts
+++ src/app/api/levels/[id]/hint/route.ts
@@ -1,25 +1,23 @@
-import { getHint } from '@/lib/data';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import { getHintByLength } from '@/lib/data';
 
 export async function POST(
-  request: Request,
+  request: NextRequest,
   { params }: { params: Promise<{ id: string }> }
-) {
-  const { id } = await params;
+): Promise<NextResponse> {
   try {
+    const { id } = await params;
     const levelId = parseInt(id, 10);
     if (isNaN(levelId)) {
       return new NextResponse('Некорректный ID уровня', { status: 400 });
     }
 
-    const { foundWords } = await request.json();
-
-    if (!Array.isArray(foundWords)) {
-      return new NextResponse('Некорректный формат найденных слов', { status: 400 });
+    const { foundWords, length } = await request.json();
+    if (!Array.isArray(foundWords) || typeof length !== 'number') {
+      return new NextResponse('Некорректный формат запроса', { status: 400 });
     }
 
-    const hint = await getHint(levelId, foundWords);
-
+    const hint = await getHintByLength(levelId, foundWords, length);
     if (hint) {
       return NextResponse.json({ hint });
     } else {
